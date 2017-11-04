3481906410d380c74984afced5952ebe94a2762a
yizhi.li <tgxpuisb@hotmail.com>
2017-11-04 21:39:32 +0800

    add add-autumn-cli

---
+++ bin/autumn.js
@@ -0,0 +1,50 @@
+#!/usr/bin/env node
+const fs = require('fs')
+const argv = require('yargs').argv
+
+const convert = require('../index')
+
+if (argv.i) {
+    if (fs.existsSync(argv.i)) {
+        fs.readFile(argv.i, (err, content) => {
+            let {
+                html,
+                css,
+                js
+            } = parseTemplate(content.toString())
+            convert(html, css, js).then(result => {
+                let path = argv.o
+                if (!path) {
+                    path = argv.i.replace(/\.autumn|\.vue$/, '.json')
+                }
+                fs.writeFile(path, JSON.stringify(result, undefined, 4), err => {
+                    if (err) {
+                        console.log(err)
+                    }
+                })
+            })
+        })
+    } else {
+        console.error('input file is not exists')
+    }
+}
+
+function parseTemplate (content) {
+    let html = content.substring(
+        content.indexOf('<template') + '<template>'.length,
+        content.indexOf('</template>')
+    )
+    let css = content.substring(
+        content.indexOf('<style>') + '<style>'.length,
+        content.indexOf('</style>')
+    )
+    let js = content.substring(
+        content.indexOf('<script>') + '<script>'.length,
+        content.indexOf('</script>')
+    )
+    return {
+        html,
+        css,
+        js
+    }
+}
